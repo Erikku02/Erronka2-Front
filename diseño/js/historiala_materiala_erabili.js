7ad987ef55725e11c13d9673fb083f9eccd78a0b
ivillanuevaa20@fpsanjorge.com <ivillanuevaa20@fpsanjorge.com>
2024-05-15 11:02:26 +0200

    buscar por alumno material_historiala

--- diseño/js/historiala_materiala_erabili.js
+++ diseño/js/historiala_materiala_erabili.js
@@ -16,9 +16,37 @@ new Vue({
         guardarRegistro: [],
         registros: [],
         stockTotala: 0,
+        listaLangile: [],
+        izenaSortu: ""
     },
     methods: {
+        async cargaLangile() {
+            try {
+                const response = await fetch(window.ruta +'langilearuta', {
+                    // const response = await fetch('https://www.talde3-back.edu/Erronka2/laravel_e2t3/public/api/langilearuta', {
+                    method: 'GET',
+                    // mode: "no-cors",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                });
+                if (!response.ok) {
+                    console.log('Errorea eskera eskaera egiterakoan');
+                    throw new Error('Errorea eskaera egiterakoan');
+                }
 
+                const datuak = await response.json();
+                // this.listaTalde = datuak
+                //      .filter(talde => talde.deleted_at === null || talde.deleted_at === "0000-00-00 00:00:00");
+                this.listaLangile = datuak
+                    .filter(langile => langile.deleted_at === null || langile.deleted_at === "0000-00-00 00:00:00");
+                    //  console.log(this.listaLangile)
+            } catch (error) {
+                console.error('Errorea: ', error);
+            }
+            this.filtrarAlumnosPorGrupo();
+        },
         async cargaMateriala() {    
             
             try {
@@ -70,14 +98,15 @@ new Vue({
             }
         },
         actualizarListaFiltrada() {
-            if (!this.materialaIzenaSortu) {
+            if (!this.materialaIzenaSortu && !this.izenaSortu) {
                 this.listaFiltrada = this.listaMaterialaErabili
                     .sort((a, b) => new Date(b.hasiera_data) - new Date(a.hasiera_data));
             } else {
                 this.listaFiltrada = this.listaMaterialaErabili
                     .sort((a, b) => new Date(b.hasiera_data) - new Date(a.hasiera_data))
                     .filter(registro => 
-                        (!this.materialaIzenaSortu || (registro.materiala && registro.materiala.id == this.materialaIzenaSortu))
+                        (!this.materialaIzenaSortu || (registro.materiala && registro.materiala.id == this.materialaIzenaSortu)) &&
+                        (!this.izenaSortu || (registro.langilea && registro.langilea.id == this.izenaSortu))
                     );
             }
         }
@@ -85,11 +114,16 @@ new Vue({
     watch:{
         materialaIzenaSortu: function () {
             this.actualizarListaFiltrada();
+        },
+        izenaSortu: function () {
+            this.actualizarListaFiltrada();
         }
     },
 
     mounted() {
         this.cargaMateriala();
         this.cargaMaterialaErabili();
+        this.cargaLangile(); // Llamar al método para cargar los alumnos
+
     }
 });
